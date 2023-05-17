177c1b69f9acd0d91af3377794e7c35640fc8d03
Dimitri <habimm@pm.me>
2023-05-17 15:00:09 +0200

    load article from tagesschau

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+async function getArticle() {
+  // const url = 'https://www.tagesschau.de/ausland/asien/jemen-buergerkrieg-waffenruhe-baerbock-100.html';
+  const url = 'https://www.tagesschau.de/inland/urteil-gruenes-gewoelbe-100.html';
+  let newsText = [];
+
+  const response = await axios.get(url);
+  const $ = cheerio.load(response.data);
+
+  const ancestor = $('article.container.content-wrapper__group');
+
+  const classes = ['seitenkopf__headline', 'meldung__subhead', 'textabsatz', 'tag-btn'];
+
+  ancestor.find('h1, h2, p, a').each((i, element) => {
+    const elClass = $(element).attr('class') || '';
+    if(classes.some(cls => elClass.includes(cls))) {
+      newsText.push($(element).text().trim());
+    }
+  });
+
+  ancestor.find('p, h2').each(() => {
+    newsText.push('');
+  });
+
+  return newsText.join('\n');
+}
 
 function App() {
+  const [article, setArticle] = useState('');
+
+  useEffect(() => {
+    (async function fetchArticle() {
+      const articleText = await getArticle();
+      setArticle(articleText);
+    })();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <pre>{article}</pre>
       </header>
     </div>
   );
