18a862e85ef79381b5a102e714cd1d37e7d8dbe9
Dimitri <habimm@pm.me>
2023-05-19 16:24:35 +0200

    remove some functiosn

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ async function getArticleText(articleUrl) {
   return newsText.join('\n');
 }
 
-async function fetchLink(articles, setArticles, openaiApiKeyRef, promptRef) {
+async function addArticle(articles, setArticles, openaiApiKeyRef, promptRef) {
   const baseUrl = 'https://www.tagesschau.de';
   const response = await axios.get(baseUrl);
   const $ = cheerio.load(response.data);
@@ -53,70 +53,52 @@ async function fetchLink(articles, setArticles, openaiApiKeyRef, promptRef) {
     summary: null,
   };
 
-  const newArticles = [...articles, article];
-  summarizeArticle(newArticles, setArticles, openaiApiKeyRef, promptRef);
+  // Summarize the article's text, or do whatever the prompt says.
+
+  const openaiApiKey = openaiApiKeyRef.current.value;
+  const prompt = promptRef.current.value;
+
+  var articleUrl = article['url'];
+  var articleText = await getArticleText(articleUrl);
+  articleText = articleText.trim();
+  articleText = articleText.substring(0, 5500);
+
+  let articleWithPrompt = prompt;
+  articleWithPrompt += "\n\n";
+  articleWithPrompt += articleText;
+
+  // The newline at the end of the prompt is ESSENTIAL. Without it, the model might ignore the prompt
+  // at the beginning of the user content's message and just complete the last sentence
+  // in the scraped article.
+  articleWithPrompt += "\n";
+
+  try {
+    const openaiBody = {
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'user', content: articleWithPrompt }],
+    };
+    console.log("Sent to OpenAI:");
+    console.log(openaiBody);
+
+    const headers = {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${openaiApiKey}`,
+    };
+
+    const response = await axios.post("https://api.openai.com/v1/chat/completions", openaiBody, {headers: headers});
+    const computedSummary = response.data.choices[0].message.content;
+
+    article['summary'] = computedSummary;
+    const newArticles = [...articles, article];
+    setArticles(newArticles);
+
+  } catch (error) {
+    console.error(`An error occurred during the API call: ${error}`);
+    window.alert(`An error occurred. Could the OpenAI API Key be incorrect?\nError: ${error}`);
+  }
 
   return article;
 }
-
-const summarizeArticle = async (articles, setArticles, openaiApiKeyRef, promptRef) => {
-  for (let index = 0; index < articles.length; index++) {
-    var article = articles[index];
-    console.log(article);
-
-    if (article.summary) {
-      console.log("Already summarized article!");
-      continue;
-    }
-
-    const openaiApiKey = openaiApiKeyRef.current.value;
-    const prompt = promptRef.current.value;
-
-    console.log("OpenAI API Key:", openaiApiKey);
-    console.log("Prompt:", prompt);
-
-    var articleUrl = article['url'];
-    var articleText = await getArticleText(articleUrl);
-    articleText = articleText.trim();
-    articleText = articleText.substring(0, 5500);
-
-    let articleWithPrompt = prompt;
-    articleWithPrompt += "\n\n";
-    articleWithPrompt += articleText;
-
-    // The newline at the end of the prompt is ESSENTIAL. Without it, the model might ignore the prompt
-    // at the beginning of the user content's message and just complete the last sentence
-    // in the scraped article.
-    articleWithPrompt += "\n";
-
-    try {
-      const openaiBody = {
-        model: 'gpt-3.5-turbo',
-        messages: [{ role: 'user', content: articleWithPrompt }],
-      };
-      console.log(openaiBody);
-
-      const headers = {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${openaiApiKey}`,
-      };
-
-      const response = await axios.post("https://api.openai.com/v1/chat/completions", openaiBody, {headers: headers});
-      const computedSummary = response.data.choices[0].message.content;
-
-      const newArticles = [...articles];
-      newArticles[index]['summary'] = computedSummary;
-      setArticles(newArticles);
-
-      break;
-    } catch (error) {
-      console.error(`An error occurred during the API call: ${error}`);
-      break;
-    }
-  }
-
-  console.log("ALL DONE.")
-};
 
 function App() {
   var stuffFromLocalStorage = JSON.parse(localStorage.getItem('articles')) || [];
@@ -126,12 +108,18 @@ function App() {
 
   useEffect(() => {
     console.log(articles);
+    console.log(process.env.REACT_APP_OPENAI_API_KEY);
     localStorage.setItem('articles', JSON.stringify(articles));
   }, [articles]);
 
+  const handleDefaultPromptClick = () => {
+    const defaultPrompt = "FASSE IN EINEM EINZIGEN SATZ FÜR EINEN 12-JÄHRIGEN ZUSAMMEN. MIT HÖCHSTENS 25 WORTEN!";
+    promptRef.current.value = defaultPrompt;
+  };
+
   return (
     <div className="App night-sky-background">
-      <button type="button" className="btn btn-danger" onClick={() => fetchLink(articles, setArticles, openaiApiKeyRef, promptRef)}>
+      <button type="button" className="btn btn-danger" onClick={() => addArticle(articles, setArticles, openaiApiKeyRef, promptRef)}>
         Add article
       </button>
 
@@ -144,9 +132,14 @@ function App() {
             </div>
           </div>
           <div className="col">
-            <div className="form-group">
-              <label htmlFor="prompt" className="custom-label">Prompt</label>
-              <input type="text" className="form-control" id="prompt" placeholder="FASSE IN EINEM EINZIGEN SATZ FÜR EINEN 12-JÄHRIGEN ZUSAMMEN. MIT HÖCHSTENS 25 WORTEN!" ref={promptRef} />
+            <div className="row">
+              <div className="form-group">
+                <label htmlFor="prompt" className="custom-label">Prompt</label>
+                <input type="text" className="form-control" id="prompt" ref={promptRef} />
+              </div>
+              <div className="col">
+                <button type="button" className="btn btn-primary" onClick={handleDefaultPromptClick}>Default Prompt</button>
+              </div>
             </div>
           </div>
         </div>
